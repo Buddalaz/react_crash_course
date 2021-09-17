5d19bd7f18af6bb36b0df13f33f080f4656b3d14
sanduns <sanduns@misynergy.com>
2021-09-17 23:31:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
+  const name = "Sandun";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello World!</h1>
+      <h2>Hello {2 + 3}</h2>
+      <h3>Hello {2 + 4 === 6 ? name : "User"}</h3> */}
+      <Header />
     </div>
   );
 }
+
+// we can create arrow base function component also
+// const App = () => {
+//   const name = "Sandun";
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//       <h2>Hello {2 + 3}</h2>
+//       <h3>Hello {2 + 4 === 6 ? name : "User"}</h3>
+//     </div>
+//   );
+// }
+
+// we can create class base component also
+// class App extends React.Component{
+// render(){
+// return <h1>Hello Friends!!</h1>
+// }
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h2>Welcome</h2>
+            <h1>Joke Generator</h1>
+        </header>
+        //we call this fragment
+        // <>
+        //     <div>dndlkv</div>
+        // </>
+    )
+}
+
+export default Header
